e62c3768017f6659c213ec785c93793d238f2c82
Kamarya <kamaryaa12@gmail.com>
2023-01-22 13:24:10 -0500

    selenium project

--- src/test/java/com/cydeo/tests/review/week1/Navigations.java
+++ src/test/java/com/cydeo/tests/review/week1/Navigations.java
@@ -1,4 +1,27 @@
 package com.cydeo.tests.review.week1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Navigations {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://www.tesla.com");
+
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
+
+
+
+
+
+    }
 }
